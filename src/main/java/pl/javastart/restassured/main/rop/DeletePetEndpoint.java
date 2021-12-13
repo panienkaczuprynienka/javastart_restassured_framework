be272c2650e1400f92efdc1f581bff2bbaa70720
vlcnh <vlcnh@gmail.com>
2021-12-13 20:05:51 +0100

    delete pet F.7

---
+++ src/main/java/pl/javastart/restassured/main/rop/DeletePetEndpoint.java
@@ -0,0 +1,44 @@
+package pl.javastart.restassured.main.rop;
+
+import org.apache.http.HttpStatus;
+import pl.javastart.restassured.main.pojo.ApiResponse;
+import pl.javastart.restassured.main.request.configuration.RequestConfigurationBuilder;
+
+import java.lang.reflect.Type;
+
+import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DeletePetEndpoint extends BaseEndpoint<DeletePetEndpoint, ApiResponse> {
+
+  private int petId;
+
+  @Override
+  protected Type getModelType() {
+    return ApiResponse.class;
+  }
+
+  @Override
+  public DeletePetEndpoint sendRequest() {
+    response = given()
+            .spec(RequestConfigurationBuilder.getDefaultRequestSpecification())
+            .pathParam("petId", petId).when().delete("/pet/{petId}");
+    return this;
+  }
+
+  @Override
+  protected int getSuccessStatusCode() {
+    return HttpStatus.SC_NOT_FOUND;
+  }
+
+  @Override
+  public DeletePetEndpoint assertStatusCode(int statusCode) {
+    assertThat(response.getStatusCode()).as("Status code").isEqualTo(statusCode);
+    return this;
+  }
+
+  public DeletePetEndpoint setPetId(int petId) {
+    this.petId = petId;
+    return this;
+  }
+}
